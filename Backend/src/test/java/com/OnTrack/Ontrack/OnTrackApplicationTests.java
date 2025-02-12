7881ce9e537ef0464220816f0aae32f76a1a2e66
Marciotavs <marciomossoro2001@gmail.com>
2025-02-12 20:25:37 +0100

    first commit

---
+++ Backend/src/test/java/com/OnTrack/Ontrack/OnTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnTrack.Ontrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
